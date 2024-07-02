f690ac67a9495b526942e17dcfbe0288e1cc743d
SA030 <suna_0132@naver.com>
2024-07-02 21:14:04 +0900

    get/set

--- app/src/main/java/bitcamp/project2/controller/AppointmentController.java
+++ app/src/main/java/bitcamp/project2/controller/AppointmentController.java
@@ -1,4 +1,31 @@
 package bitcamp.project2.controller;
 
+import bitcamp.project2.vo.Plan;
+import bitcamp.project2.vo.User;
+
+import java.util.LinkedList;
+
 public class AppointmentController {
+    PlanController pc = PlanController.getInstance();
+    UserController uc = UserController.getInstance();
+
+    LinkedList<Plan> planList = pc.getPlanList();
+    LinkedList<User> userList = uc.getUserList();
+
+    ///////////////////////////////////////////////////////////
+    ////////////////////// Constructor ////////////////////////
+    ///////////////////////////////////////////////////////////
+    AppointmentController(){
+
+    }
+
+
+
+
+    ///////////////////////////////////////////////////////////
+    ///////////////////////// Method //////////////////////////
+    ///////////////////////////////////////////////////////////
+    public void menu(){
+
+    }
 }
